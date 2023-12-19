4216d994adada13d351f9e81c0655d3ef7397bd5
waverly <wf1095870395@gmail.com>
2023-12-19 19:47:29 +0800

    增加文件夹最大高度限制和折叠展开功能

--- src/newtab/components/bookmark-component.vue
+++ src/newtab/components/bookmark-component.vue
@@ -1,57 +1,73 @@
 <template>
-  <div class="bookmark-card" :style="{ backgroundColor: randomBackgroundColor,width:width}">
-    <div class="card-header" style="mix-blend-mode: difference;color: white">
-      |&nbsp;&nbsp;{{ title }}
+  <div
+    class="bookmark-card"
+    :style="{
+      backgroundColor: randomBackgroundColor,
+      width: width,
+      maxHeight: isExpanded ? 'none' : maxHeight,
+    }"
+    ref="bookmarkListRef"
+  >
+    <div class="card-header" style="mix-blend-mode: difference; color: white">
+      <el-row justify="space-between">
+        <a>|&nbsp;&nbsp;{{ title }}</a>
+        <el-button v-if="isContentOverflowing" @click="toggleExpand">
+          <div v-if="isExpanded">
+            收起<el-icon><CaretTop /></el-icon>
+          </div>
+          <div v-else>
+            展开<el-icon><CaretBottom /></el-icon>
+          </div>
+        </el-button>
+      </el-row>
     </div>
-    <div class="bookmark-list">
-      <div v-for="(item,index) in bookmarksData" :key="index" class="bookmark-item">
 
-
-        <el-text v-if="item.url" class="bookmark-text"
-                 truncated
-                 style="mix-blend-mode: difference;color: white;"
-                 @click="locationToUrl(item.url)"
-
+    <div class="bookmark-list" :style="bookmarkListStyle">
+      <div v-for="(item, index) in bookmarksData" :key="index" class="bookmark-item">
+        <el-text
+          v-if="item.url"
+          class="bookmark-text"
+          truncated
+          style="mix-blend-mode: difference; color: white"
+          @click="locationToUrl(item.url)"
         >
           <el-icon size="14px">
-            <Link/>
+            <Link />
           </el-icon>
           {{ item.title }}
         </el-text>
-
-
-        <sub-bookmark-component v-else :bookmarks-data="item.children" :title="item.title"
-                                :width="width-'20px'"></sub-bookmark-component>
-
+        <sub-bookmark-component
+          v-else
+          :bookmarks-data="item.children"
+          :title="item.title"
+          :width="width - '20px'"
+        ></sub-bookmark-component>
       </div>
-
     </div>
   </div>
 </template>
 
 <script setup>
-import {ref} from 'vue';
-import {Folder, Link} from "@element-plus/icons-vue";
+import { ref, computed, onMounted, nextTick, watch } from "vue";
+import { CaretBottom, CaretTop, Link } from "@element-plus/icons-vue";
 import SubBookmarkComponent from "@/newtab/components/sub-bookmark-component.vue";
 
 const props = defineProps({
   title: "",
   bookmarksData: {
     type: Object,
-    default: () => ref({})
+    default: () => ref({}),
   },
-  cardColor: '#000000',
-  textColor: '#ffffff',
-  width: '220px',
+  cardColor: "#000000",
+  textColor: "#ffffff",
+  width: "220px",
+  maxHeight: "300px",
 });
 
-const colorArray = [
-  '#414b4b',
-  '#383c51',
-  '#3f534b',
-  '#505e60',
-  '#3a3a3a',
-  '#443a3a'];
+const isExpanded = ref(false);
+const maxHeight = ref(props.maxHeight);
+
+const colorArray = ["#414b4b", "#383c51", "#3f534b", "#505e60", "#3a3a3a", "#443a3a"];
 // Function to get a random color from the array
 const getRandomColor = () => {
   const randomIndex = Math.floor(Math.random() * colorArray.length);
@@ -73,27 +89,75 @@ function locationToUrl(url) {
   window.open(url);
 }
 
+const bookmarkListRef = ref(null);
+const showExpandButton = computed(() => {
+  if (!bookmarkListRef.value) return false;
+  const actualHeight = bookmarkListRef.value.scrollHeight;
+  const maxHeightInPixels = parseInt(props.maxHeight);
+  console.info(actualHeight, maxHeightInPixels);
+  return actualHeight > maxHeightInPixels;
+});
+
+const isContentOverflowing = computed(() => {
+  if (!bookmarkListRef.value) return false;
+  return bookmarkListRef.value.scrollHeight > parseInt(props.maxHeight);
+});
+
+// 根据是否折叠和内容是否超过maxHeight来动态设置样式
+const bookmarkListStyle = computed(() => {
+  return {
+    maxHeight:
+      isExpanded.value || !isContentOverflowing.value
+        ? "none"
+        : parseInt(props.maxHeight) - 45 + "px",
+    overflowY: "auto",
+  };
+});
+
+// 点击展开/折叠时触发
+const toggleExpand = () => {
+  isExpanded.value = !isExpanded.value;
+};
+
+watch(
+  () => props.maxHeight,
+  () => {
+    if (bookmarkListRef.value) {
+      isContentOverflowing.value =
+        bookmarkListRef.value.scrollHeight > parseInt(props.maxHeight);
+    }
+  }
+);
+
+onMounted(async () => {
+  await nextTick();
+});
 </script>
 
 <style scoped lang="scss">
-
 .bookmark-card {
   height: auto;
   border-radius: 10px;
   padding: 5px;
   margin: 10px;
-  overflow: auto;
 
   .card-header {
     font-size: 1em;
     font-weight: bold;
     margin: 5px;
   }
+  .card-header button {
+    background: transparent;
+    color: white;
+    border: none;
+    height: 21.47px;
+  }
 
   .bookmark-list {
     padding: 0;
     color: white;
-    overflow: scroll;
+
+    overflow-y: auto;
 
     .bookmark-item {
       height: auto;
@@ -101,9 +165,8 @@ function locationToUrl(url) {
       width: auto;
       transition: all 0.3s ease; /* 为下划线添加平滑过渡效果 */
 
-
       &:hover {
-        background-color: #484848C1;
+        background-color: #484848c1;
         border-radius: 4px;
         cursor: pointer;
       }
@@ -114,7 +177,4 @@ function locationToUrl(url) {
 :deep(.bookmark-text:hover) {
   text-decoration: underline; /* 鼠标悬停时添加下划线 */
 }
-
-
-/* Add additional styling as needed */
 </style>
